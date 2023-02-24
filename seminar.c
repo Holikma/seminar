fa66f26f924d2e01335a84099830133e9e3ece54
Marek Holik <maresidlo@gmail.com>
2023-02-24 13:33:54 +0100

    pointers

--- seminar.c
+++ seminar.c
@@ -1,23 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-
-#define l 4
-#define k 4
-
-
-void prienik(int a[l], int b[k]){
-    for (int i = 0; i < l; i++){
-        for (int j = 0; j < k; j++){
-            if (a[i] == b[j]){
-                printf("%d ", a[i]);
-            }
-        }
-    }
-}
-
-int main(){
-    int x[l] = {1,2,3,4};
-    int y[k] = {-1, 3, 5, 7};
-    prienik(x,y);
+int main()
+{
+   short int i = 1;
+   short int*c = &i;
+   if (*c != 0)   
+       printf("Little endian");
+   else
+       printf("Big endian");
+   return 0;
 }
